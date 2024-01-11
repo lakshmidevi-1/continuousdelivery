c0704e7ca0d094fc6fcfd3a7cc82aa64c02cd71c
lakshmidevi-1 <maddinenilakshmidevi1@gmail.com>
2024-01-11 09:59:25 +0530

    first commit

---
+++ Operation.java
@@ -0,0 +1,8 @@
+public class Operation
+{
+	public static void main(String[] args) {
+		int num1=55,num2=15;
+		System.out.println("Addition of two numbers :"+(num1+num2));
+		System.out.println("Subtraction of two numbers :"+(num1-num2));
+	}
+}
